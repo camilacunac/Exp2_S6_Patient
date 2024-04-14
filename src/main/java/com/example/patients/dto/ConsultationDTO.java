f15e4e79dc76769f28964801a2b05686060bee45
Camilo Acuna <camilo.acuna@orbis-dev.cl>
2024-04-14 17:47:48 -0400

    add consultation dto

---
+++ src/main/java/com/example/patients/dto/ConsultationDTO.java
@@ -0,0 +1,42 @@
+package com.example.patients.dto;
+
+import java.util.Date;
+
+public class ConsultationDTO {
+    private String motivo;
+    private Date fecha;
+    private String diagnostico;
+    private String tratamiento;
+
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getDiagnostico() {
+        return diagnostico;
+    }
+
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+
+    public String getTratamiento() {
+        return tratamiento;
+    }
+
+    public void setTratamiento(String tratamiento) {
+        this.tratamiento = tratamiento;
+    }
+}
