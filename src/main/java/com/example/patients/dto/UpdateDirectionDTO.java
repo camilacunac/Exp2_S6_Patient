1ca8c62961cfce2518ac768545e598ca4c7b4dc8
Camilo Acuna <camilo.acuna@orbis-dev.cl>
2024-04-14 17:47:34 -0400

    add update direction dto

---
+++ src/main/java/com/example/patients/dto/UpdateDirectionDTO.java
@@ -0,0 +1,13 @@
+package com.example.patients.dto;
+
+public class UpdateDirectionDTO {
+    private String direccion;
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
