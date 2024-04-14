bfeb62f9c24f6e378b10d1c8ca3893c1ae399f71
Camilo Acuna <camilo.acuna@orbis-dev.cl>
2024-04-14 17:47:58 -0400

    add attention dto

---
+++ src/main/java/com/example/patients/dto/AttentionDTO.java
@@ -0,0 +1,42 @@
+package com.example.patients.dto;
+
+import java.util.Date;
+
+public class AttentionDTO {
+    private String tipo;
+    private Date fecha;
+    private String diagnostico;
+    private String tratamiento;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getDiagnostico() {
+        return diagnostico;
+    }
+
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+
+    public String getTratamiento() {
+        return tratamiento;
+    }
+
+    public void setTratamiento(String tratamiento) {
+        this.tratamiento = tratamiento;
+    }
+}
